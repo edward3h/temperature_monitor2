d00e4e57d32f575332cf2e32463d544aaeb63049
Edward Harman <eharman@tripadvisor.com>
2020-08-06 09:45:18 -0400

    have to update the send api here too

--- src/test/groovy/org/ethelred/temperature_monitor2/ManualTestMailer.groovy
+++ src/test/groovy/org/ethelred/temperature_monitor2/ManualTestMailer.groovy
@@ -2,10 +2,11 @@
 package org.ethelred.temperature_monitor2
 
 /**
+ * I haven't figured out a way to automatically test email yet, so I run this process manually
  */
 class ManualTestMailer {
     static void main(String[] args) {
-        new Mail(subject: "Testing Email", body: "please ignore")
-        .send()
+        new Mail()
+                .send(subject: "Testing Email", body: "please ignore")
     }
 }
